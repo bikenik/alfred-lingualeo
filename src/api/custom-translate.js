ef882fbd52d6f7d878c219f293ff722f6a370605
bikenik <bikeniks@gmail.com>
2018-12-10 20:53:35 +0300

    added subtitle with additional info to custom-translate

--- src/api/custom-translate.js
+++ src/api/custom-translate.js
@@ -4,10 +4,12 @@ const alfy = require('alfy')
 
 const search = JSON.parse(process.env.search)
 search.translate_value = alfy.input
+const subtitle = 'your version: If you want to add a sentence, put a punctuation mark at the end (. ? !)'
 alfy.output([{
 	title: process.env.currentSearch,
-	subtitle: 'type or paste your own version of translate',
+	subtitle,
 	variables: {
 		search: JSON.stringify(search)
-	}
+	},
+	text: {largetype: `${process.env.currentSearch}\n\n💡${subtitle}`}
 }])
